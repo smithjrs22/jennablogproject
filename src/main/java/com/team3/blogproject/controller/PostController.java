bcab4d23d5361a428f3edc2ad97cea8ef8806cac
kmiller <kmmiller418@gmail.com>
2022-06-09 10:45:11 -0400

    Create CRUD operations for Posts, create blank forms

--- src/main/java/com/team3/blogproject/controller/PostController.java
+++ src/main/java/com/team3/blogproject/controller/PostController.java
@@ -1,4 +1,67 @@
 package com.team3.blogproject.controller;
 
+import com.team3.blogproject.model.Post;
+import com.team3.blogproject.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        return findPaginated(1, model);
+    }
+
+    @GetMapping("/showNewPostForm")
+    public String showNewPostForm(Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "forms/create_post";
+    }
+
+    @PostMapping("/savePost")
+    public String savePost(@ModelAttribute("post") Post post) {
+        postService.savePost(post);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+        Post post = postService.getPostById(id);
+        model.addAttribute("post", post);
+        return "forms/update_post";
+    }
+
+    @GetMapping("/deletePost/{id}")
+    public String deletePost(@PathVariable(value = "id") long id) {
+        this.postService.deletePostById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                Model model) {
+        int pageSize = 3;
+        Page<Post> page = postService.findPaginated(pageNo, pageSize);
+        List<Post> listPosts = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("listPosts", listPosts);
+        return "index";
+    }
 }

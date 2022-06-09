719f265d1f14ced81bce868160c27a2fa637acb6
kmiller <kmmiller418@gmail.com>
2022-06-09 09:00:08 -0400

    Creates project skeleton, initialize security config with permitAll()

---
+++ src/test/java/com/team3/blogproject/BlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.blogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

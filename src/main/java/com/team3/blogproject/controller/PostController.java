e8602fd80774aef1d98fbcdec0515a778fd06b0f
kmiller <kmmiller418@gmail.com>
2022-06-09 10:39:33 -0400

    Create PostService and implementation

---
+++ src/main/java/com/team3/blogproject/controller/PostController.java
@@ -0,0 +1,4 @@
+package com.team3.blogproject.controller;
+
+public class PostController {
+}
---
+++ src/main/java/com/team3/blogproject/service/PostServiceImpl.java
@@ -0,0 +1,48 @@
+package com.team3.blogproject.service;
+
+import com.team3.blogproject.model.Post;
+import com.team3.blogproject.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostServiceImpl implements PostService{
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Override
+    public void savePost(Post post) {
+        this.postRepository.save(post);
+    }
+
+    @Override
+    public Post getPostById(long id) {
+        Optional<Post> optional = postRepository.findById(id);
+        Post post = null;
+        if(optional.isPresent()){
+            post = optional.get();
+        } else {
+            throw new RuntimeException("Post not found with ID ::"+id);
+        } return post;
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        this.postRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Post> findPaginated(int pageNum, int pageSize) {
+        Sort sort = Sort.by("id").descending();
+
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+        return this.postRepository.findAll(pageable);
+    }
+}

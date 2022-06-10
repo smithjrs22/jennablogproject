8ec27954f77510b32048d8661531ff2a4d37584a
smithjrs22 <101147147+smithjrs22@users.noreply.github.com>
2022-06-10 15:08:55 -0400

    Update BlogProjectApplication.java

--- src/main/java/com/team3/blogproject/BlogProjectApplication.java
+++ src/main/java/com/team3/blogproject/BlogProjectApplication.java
@@ -2,10 +2,9 @@ package com.team3.blogproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-// @ComponentScan
 public class BlogProjectApplication {
 
     public static void main(String[] args) {

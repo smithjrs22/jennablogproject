09fb29ade786e39245602f8f95c5cbb8a1cd6fa9
kmmiller418 <100967540+kmmiller418@users.noreply.github.com>
2022-06-10 09:44:25 -0400

    Update HomeController.java
    removed conflicting post routes

--- src/main/java/com/team3/blogproject/controllers/HomeController.java
+++ src/main/java/com/team3/blogproject/controllers/HomeController.java
@@ -20,25 +20,6 @@ public class HomeController {
 @Autowired
 private PostService postService;
 
-@RequestMapping("/posts/view/{id}")
-public String view(@PathVariable("id") Long id, Model model) {
-    Post post = postService.findById(id);
-    model.addAttribute("post", post);
-    return "posts/view";
-}
-
-@RequestMapping("/")
-public String index(Model model) {
-List<Post> latest5Posts = postService.findLatest5();
-model.addAttribute("lastest5post", latest5Posts);
-
-
-List<Post> latest3Posts = latest5Posts.stream()
-.limit(3).collect(Collectors.toList());
-model.addAttribute("latest3posts", latest3Posts);
-
-return "index";
-}
 
 @GetMapping("/login")
 public String login(){

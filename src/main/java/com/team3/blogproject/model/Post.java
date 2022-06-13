73cb7eee786d3d31203a4c1a8d0c64a425ae4ea7
MikeLMeese <101145367+MikeLMeese@users.noreply.github.com>
2022-06-13 09:26:55 -0400

    Readded validation annotations

--- src/main/java/com/team3/blogproject/model/Post.java
+++ src/main/java/com/team3/blogproject/model/Post.java
@@ -1,8 +1,8 @@
 package com.team3.blogproject.model;
-
 import java.util.Date;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name="post")
@@ -10,14 +10,15 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     
     @Column(name="date")
     private Date date;
 
+    @NotEmpty(message = "Please fill out the required field")
     @Column(name="title")
     private String title;
 
+    @NotEmpty(message = "Please fill out the required field")
     @Column(name="content")
     private String content;
 
@@ -61,5 +62,15 @@ public class Post {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + content + '\'' +
+                ", date=" + date +
+                '}';
     }
 }

896d734f3ab6cf63799a26c4bcbb8c381a79a472
MikeLMeese <101145367+MikeLMeese@users.noreply.github.com>
2022-06-15 14:19:02 -0400

    Everything is pain

--- src/main/java/com/team3/blogproject/config/SecurityConfiguration.java
+++ src/main/java/com/team3/blogproject/config/SecurityConfiguration.java
@@ -3,9 +3,11 @@ package com.team3.blogproject.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,20 +42,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        
         http
                 .authorizeRequests()
-                .antMatchers(
+                .antMatchers(HttpMethod.GET,
                         "/",
                         "/posts/view/**",
                         "/registration**",
+                        "/resources/**",
                         "/public/**",
                         "/js/**",
                         "/css/**",
-                        "/**/*.js",
-                        "/**/*.css",
-                        "/img/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
+                        "/img/**",
+                        "/webjars/**").permitAll()
+                .anyRequest().permitAll();
+                
+            http
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
